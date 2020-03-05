30e859bcb5865d7407a867ec2fe2d7da1c7d585f
jchemell <juanchemell@gmail.com>
2020-03-05 17:01:47 -0300

    Carga de main con 4 productos.

---
+++ js/index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const title = document.querySelector('h1');
+const main = document.querySelectorAll('.mainDivs');
+const btnLoginLogin = document.querySelector('#loginLogin');
+const btnLoginSignup = document.querySelector('#loginSignup');
+const btnSignupCancel = document.querySelector('#signupCancel');
+const btnAdd = document.querySelectorAll('.material-icons');
+
+function toggleClass(e, c) {
+	e.classList.toggle(c);
+}
+
+btnLoginLogin.addEventListener('click', () => {
+	toggleClass(main[0], 'visually-hidden');
+	toggleClass(main[2], 'visually-hidden');
+
+	toggleClass(title, 'homepageSize');
+});
+
+btnLoginSignup.addEventListener('click', () => {
+	toggleClass(main[0], 'visually-hidden');
+	toggleClass(title, 'homepageSize');
+	toggleClass(main[1], 'visually-hidden');
+});
+
+btnSignupCancel.addEventListener('click', () => {
+	toggleClass(main[0], 'visually-hidden');
+	toggleClass(title, 'homepageSize');
+	toggleClass(main[1], 'visually-hidden');
+});
+
+for (const btn of btnAdd) {
+	btn.addEventListener('click', () => {
+		if (btn.innerHTML == 'add_circle') {
+			btn.innerHTML = 'done_outline';
+		} else btn.innerHTML = 'add_circle';
+	});
+}
