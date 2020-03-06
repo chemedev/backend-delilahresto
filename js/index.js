3dbe9cdf39403db7630972d1344451bd285ca2ef
jchemell <juanchemell@gmail.com>
2020-03-06 16:39:01 -0300

    Sección confirmar pedido, sin terminar.

--- js/index.js
+++ js/index.js
@@ -1,41 +1,58 @@
 'use strict';
 
-const title = document.querySelector('h1');
-const main = document.querySelectorAll('.mainDivs');
+let dishesCount = 0;
+
+//! DOM manipulation
+const header = document.querySelector('header');
+const main = document.querySelector('main');
+const mainDivs = document.querySelectorAll('.mainDivs');
 const btnLoginLogin = document.querySelector('#loginLogin');
 const btnLoginSignup = document.querySelector('#loginSignup');
 const btnSignupCancel = document.querySelector('#signupCancel');
 const btnAdd = document.querySelectorAll('.material-icons');
+const wrapperFixed = document.querySelector('.wrapper--fixed');
 
+//! Generic class changer function
 function toggleClass(e, c) {
 	e.classList.toggle(c);
 }
 
-title.addEventListener('click', () => (location.href = 'index.html'));
+//! Header => Homepage
+header.addEventListener('click', () => (location.href = 'index.html'));
 
 btnLoginLogin.addEventListener('click', () => {
-	toggleClass(main[0], 'visually-hidden');
-	toggleClass(main[2], 'visually-hidden');
+	toggleClass(mainDivs[0], 'visually-hidden');
+	toggleClass(mainDivs[2], 'visually-hidden');
+	toggleClass(header, 'homepageHeader');
+	toggleClass(main, 'homepageMain');
 
-	toggleClass(title, 'homepageSize');
 });
 
 btnLoginSignup.addEventListener('click', () => {
-	toggleClass(main[0], 'visually-hidden');
-	toggleClass(title, 'homepageSize');
-	toggleClass(main[1], 'visually-hidden');
+	toggleClass(mainDivs[0], 'visually-hidden');
+	toggleClass(header, 'homepageHeader');
+	toggleClass(main, 'homepageMain');
+	toggleClass(mainDivs[1], 'visually-hidden');
 });
 
 btnSignupCancel.addEventListener('click', () => {
-	toggleClass(main[0], 'visually-hidden');
-	toggleClass(title, 'homepageSize');
-	toggleClass(main[1], 'visually-hidden');
+	toggleClass(mainDivs[0], 'visually-hidden');
+	toggleClass(header, 'homepageHeader');
+	toggleClass(main, 'homepageMain');
+	toggleClass(mainDivs[1], 'visually-hidden');
 });
 
+//! Listeners => add/remove dishes
 for (const btn of btnAdd) {
 	btn.addEventListener('click', () => {
 		if (btn.innerHTML == 'add_circle') {
 			btn.innerHTML = 'done_outline';
-		} else btn.innerHTML = 'add_circle';
+			dishesCount++;
+		} else {
+			btn.innerHTML = 'add_circle';
+			dishesCount--;
+		}
+		if (dishesCount == 0 || wrapperFixed.classList.contains('visually-hidden'))
+			toggleClass(wrapperFixed, 'visually-hidden');
 	});
 }
