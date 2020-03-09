5e832f1ea9d171ba53d7ac650c2e48946b9514c1
jchemell <juanchemell@gmail.com>
2020-03-08 22:26:38 -0300

    Done btnfollow & btnconfirm hidind icons.

--- js/index.js
+++ js/index.js
@@ -9,9 +9,12 @@ const mainDivs = document.querySelectorAll('.mainDivs');
 const btnLoginLogin = document.querySelector('#loginLogin');
 const btnLoginSignup = document.querySelector('#loginSignup');
 const btnSignupCancel = document.querySelector('#signupCancel');
-const btnAdd = document.querySelectorAll('.material-icons');
+const btnAdd = document.querySelectorAll('.material-icons--add');
 const wrapperFixed = document.querySelector('.wrapper--fixed');
 const btnOrderAdd = document.querySelector('#orderAdd');
+const btnConfirm = document.querySelector('#detailConfirm');
+const btnDelete = document.querySelectorAll('.material-icons--cancel');
+const btnFollow = document.querySelector('#successfulFollow');
 
 //! Generic class changer function
 function toggleClass(e, c) {
@@ -21,12 +24,12 @@ function toggleClass(e, c) {
 //! Header => Homepage
 header.addEventListener('click', () => (location.href = 'index.html'));
 
+//! Main Sections SHOW/HIDE
 btnLoginLogin.addEventListener('click', () => {
 	toggleClass(mainDivs[0], 'visually-hidden');
 	toggleClass(mainDivs[2], 'visually-hidden');
 	toggleClass(header, 'homepageHeader');
 	toggleClass(main, 'homepageMain');
-
 });
 
 btnLoginSignup.addEventListener('click', () => {
@@ -48,6 +51,24 @@ btnOrderAdd.addEventListener('click', () => {
 	toggleClass(mainDivs[3], 'visually-hidden');
 });
 
+btnConfirm.addEventListener('click', () => {
+	toggleClass(mainDivs[3], 'visually-hidden');
+	toggleClass(mainDivs[4], 'visually-hidden');
+});
+
+btnFollow.addEventListener('click', () => {
+	toggleClass(mainDivs[4], 'visually-hidden');
+	toggleClass(mainDivs[3], 'visually-hidden');
+
+	const button = mainDivs[3].querySelector('.wrapper');
+	toggleClass(button, 'visually-hidden');
+
+	const icons = mainDivs[3].querySelectorAll('.material-icons');
+	icons.forEach(icon => {
+		toggleClass(icon, 'visually-hidden');
+	});
+});
+
 //! Listeners => add/remove dishes
 for (const btn of btnAdd) {
 	btn.addEventListener('click', () => {
@@ -61,4 +82,8 @@ for (const btn of btnAdd) {
 		if (dishesCount == 0 || wrapperFixed.classList.contains('visually-hidden'))
 			toggleClass(wrapperFixed, 'visually-hidden');
 	});
+}
+
+for (const btn of btnDelete) {
+	btn.addEventListener('click', () => btn.parentNode.remove());
 }
