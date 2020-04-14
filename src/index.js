060569bfb21a30820be6e127e33102ee43b04941
jchemell <juanchemell@gmail.com>
2020-04-14 16:00:31 -0300

    Arranged everything in public folder

--- src/index.js
+++ src/index.js
@@ -2,11 +2,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fs = require('fs');
 
 //* INIT
 const port = 3000;
 const app = express();
 app.listen(port, console.log(`Server listening on port: ${port}`));
+
+//! HANDLER
+let handleRequest = (req, res) => {
+	res.writeHead(200, {
+		'Content-Type': 'text/html'
+	});
+	fs.readFile('src/index.html', null, function(err, data) {
+		if (err) {
+			res.writeHead(404);
+			res.write('File not found.');
+		} else {
+			res.write(data);
+		}
+		res.end();
+	});
+};
 
 //* CFG
 app.use(
@@ -19,6 +36,7 @@ app.use(
 	})
 );
 app.use(bodyParser.json());
+app.use(express.static(__dirname + 'src/../public'));
 
 //? WELL-FORMED JSON ? NEXT() : { ERR }
 app.use((err, _req, res, next) => {
@@ -30,4 +48,4 @@ app.use((err, _req, res, next) => {
 app.use(require('./routes/products'));
 app.use(require('./routes/users'));
 app.use(require('./routes/orders'));
-app.get('/', (_req, res) => res.send('Welcome to Delilah Restó'));
+app.get('/', handleRequest);
